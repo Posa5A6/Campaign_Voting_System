8f22847d583e19daa34daf1f131c998249b38736
Posa Narendra <171539501+Posa5A6@users.noreply.github.com>
2026-02-27 15:38:11 +0530

    Update settings.py

--- campaign_voting/settings.py
+++ campaign_voting/settings.py
@@ -158,17 +158,3 @@ if not DEBUG:
 
 
 
-#----------------------------------------------------
-#for creating super manaily
-
-import os
-from django.contrib.auth import User
-
-if os.environ.get("CREATE_SUPERUSER") == "True":
-    User = get_user_model()
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="admin",
-            email="admin@gmail.com",
-            password="Admin@123"
-        )
